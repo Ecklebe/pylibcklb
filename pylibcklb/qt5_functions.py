c82fc37246e09555f7d02c3f69cbf83dca8d0707
Tobias <tobias.ecklebe@outlook.de>
2017-11-10 07:49:20 +0100

    add license to file header and add new file for metadata

--- pylibcklb/qt5_functions.py
+++ pylibcklb/qt5_functions.py
@@ -15,7 +15,21 @@
 #
 # @warning      The functions need a self pointer of the qt application! 
 #
-# @copyright    Unknown at this stage of implemantation.
+# @copyright    Filtergenerator for ADTF
+#               Copyright (C) 2017  Tobias Ecklebe
+#
+#               This program is free software: you can redistribute it and/or modify
+#               it under the terms of the GNU Lesser General Public License as published by
+#               the Free Software Foundation, either version 3 of the License, or
+#               (at your option) any later version.
+#
+#               This program is distributed in the hope that it will be useful,
+#               but WITHOUT ANY WARRANTY; without even the implied warranty of
+#               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#               GNU Lesser General Public License for more details.
+#
+#               You should have received a copy of the GNU Lesser General Public License
+#               along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import os  
 import sys
