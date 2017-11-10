04152ccf832f183424e149c2c85660a46f8c7a02
Tobias <tobias.ecklebe@outlook.de>
2017-11-10 09:04:56 +0100

    fix problem with version

--- pylibcklb/__init__.py
+++ pylibcklb/__init__.py
@@ -22,7 +22,7 @@
 #               along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from .version import __version__
+from .version import *
 from .ClassLibrary import *
 from .FunctionLibrary import *
 from .math_functions import *
