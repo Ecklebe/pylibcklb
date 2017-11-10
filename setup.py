5db317d8a0f4319799ffa686cab69395047d5a4e
Tobias <tobias.ecklebe@outlook.de>
2017-11-10 07:50:04 +0100

    add license to file header and configure the setup

--- setup.py
+++ setup.py
@@ -5,29 +5,66 @@
 # @date		    02.11.2017
 # @version	    0.1.0
 # @bug          No bugs at the moment.
-# @copyright    Unknown at this stage of implemantation.
+# @copyright    pylibcklb package
+#               Copyright (C) 2017  Tobias Ecklebe
 #
-from setuptools import setup
+#               This program is free software: you can redistribute it and/or modify
+#               it under the terms of the GNU Lesser General Public License as published by
+#               the Free Software Foundation, either version 3 of the License, or
+#               (at your option) any later version.
+#
+#               This program is distributed in the hope that it will be useful,
+#               but WITHOUT ANY WARRANTY; without even the implied warranty of
+#               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#               GNU Lesser General Public License for more details.
+#
+#               You should have received a copy of the GNU Lesser General Public License
+#               along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
+# imports
+import os
+from setuptools import setup, find_packages
+from pylibcklb.metadata import Variables
+
+
+# @note Found at: https://gitlab.namibsun.net/namboy94/comunio-manager/blob/master/setup.py
 def readme():
-    with open('README.md') as f:
-        return f.read()
+    """
+    Reads the readme file.
+
+    :return: the readme file as a string
+    """
+    try:
+        # noinspection PyPackageRequirements
+        import pypandoc
+        with open('README.md') as f:
+            # Convert markdown file to rst
+            markdown = f.read()
+            rst = pypandoc.convert(markdown, 'rst', format='md')
+            return rst
+    except (OSError, ImportError):
+        # If pandoc is not installed, just return the raw markdown text
+        with open('README.md') as f:
+            return f.read()
 
 exec(open('pylibcklb/version.py').read())
 
-setup(name='pylibcklb',
+setup(name=Variables.name,
       version=__version__,
-      description='The libary for all functions and classes implemented in python from Tobias Ecklebe',
+      description=Variables.description,
       long_description=readme(),
-      author='Tobias Ecklebe',
-      author_email='tobias.ecklebe@outlook.de',
-      license='MIT',
-      packages=['pylibcklb'],
-      install_requires=[
-            'lxml',
-            'pyqt5>=5.8.2',
-            'markdown2',
-            'numpy',
-      ],
+      classifiers=Variables.classifiers,
+      url=Variables.url,
+      download_url=Variables.download_url,
+      author=Variables.author,
+      author_email=Variables.author_email,
+      license=Variables.license,
+      packages=find_packages(),
+      install_requires=Variables.install_requires,
+      extras_require=Variables.extras_require,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      #scripts=find_scripts(),
       include_package_data=True,
       zip_safe=False)
